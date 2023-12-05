f5f4f49395eec08d49adba1d7e53e78670b87b6a
Akashaf <akashaf93@gmail.com>
2023-12-05 18:22:34 +0800

    responsive layout project section

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,13 +1,15 @@
 'use client';
 
-import { Button } from '@/components/ui/button';
+import { Button, buttonVariants } from '@/components/ui/button';
 import { Card, CardContent, CardFooter, CardHeader, CardTitle } from '@/components/ui/card';
 import { motion } from 'framer-motion';
+import Link from 'next/link';
 interface Repository {
   id: number;
   name: string;
   fork: boolean;
   description: string;
+  html_url: string;
   // Add more properties as needed
 }
 const Projects = ({ repoList }: { repoList: Repository[] }) => {
@@ -30,7 +32,7 @@ const Projects = ({ repoList }: { repoList: Repository[] }) => {
         >
           projects
         </motion.h2>
-        <div className="grid grid-cols-4 gap-4">
+        <div className="grid grid-col-1 lg:grid-cols-4 gap-4">
           {repoList &&
             repoList.map(
               (repo) =>
@@ -45,14 +47,14 @@ const Projects = ({ repoList }: { repoList: Repository[] }) => {
                       <CardHeader>
                         <CardTitle className="uppercase">{repo.name.split('_').join(' ')}</CardTitle>
                       </CardHeader>
-                      <CardContent className='h-[30vh] overflow-hidden flex items-center'>
-                        <p>
-                          {repo.description ?? 'No description provided for now.'}
-                        </p>
+                      <CardContent className="h-[30vh] overflow-hidden flex items-center">
+                        <p>{repo.description ?? 'No description provided for now.'}</p>
                       </CardContent>
                       <CardFooter className="flex justify-end gap-4">
                         <motion.div whileTap={{ scale: 0.95, rotate: '2.5deg' }}>
-                          <Button variant="outline">View Project</Button>
+                          <Button  asChild>
+                            <Link href={repo.html_url}>View Project</Link>
+                          </Button>
                         </motion.div>
                       </CardFooter>
                     </Card>
